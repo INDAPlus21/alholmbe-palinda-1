ac3f5d1e874ac3aa8ff1e021501b4a4952375894
Alexander <alexanderholmberg91@gmail.com>
2022-03-10 20:19:04 +0100

    loops exc

---
+++ src/gotour_exercises/loops.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	oldZ := 0.0
+	// choose arbitrary value depending on your "fault tolerance"
+	for math.Abs(z-oldZ) > 0.000000000001 {
+		oldZ = z
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
